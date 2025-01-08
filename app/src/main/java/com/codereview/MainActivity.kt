32ca114e3c93acb17184815e33526fac568e89f8
Ermek <directoryourlabel@gmail.com>
2025-01-08 12:45:16 +0400

    BadDi3

--- app/src/main/java/com/codereview/MainActivity.kt
+++ app/src/main/java/com/codereview/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.codereview.ui.theme.CodeReviewTheme
 import com.codereview.ui.theme.VacancyList
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,12 +12,13 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel: VacanciesViewModel by viewModels()
+    //private val viewModel: VacanciesViewModel by viewModels()
+    //val viewModel: VacanciesViewModel = hiltViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CodeReviewTheme {
-
+                val viewModel: VacanciesViewModel = hiltViewModel()
                 val listVac = viewModel.result
                 VacancyList(listVac)
             }
--- app/src/main/java/com/codereview/VacanciesViewModel.kt
+++ app/src/main/java/com/codereview/VacanciesViewModel.kt
@@ -4,42 +4,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import dagger.Binds
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import com.codereview.di.VacancyRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
-import javax.inject.Singleton
 
 @HiltViewModel
-class VacanciesViewModel @Inject constructor(private val repo: DataRepository) : ViewModel() {
+class VacanciesViewModel @Inject constructor(private val repo: VacancyRepository) : ViewModel() {
     var result: List<Vacancy>  by mutableStateOf(listOf())
     init{ result = repo.vacancies() }
 }
 
-//interface DataRepository {
-//    fun vacancies(): List<Vacancy>
-//}
-class DataRepository {
-    fun vacancies() = getVacancies()
-}
-//class DataRepoImpl@Inject constructor() : DataRepository {
-//    override fun vacancies() = getVacancies()
-//}
 
-@InstallIn(SingletonComponent::class)
-@Module
-object ProfileModule {
-//    @Binds
-//    fun getProfileSource(repo: DataRepoImpl): DataRepository
 
-    @Provides
-    @Singleton
-    fun providesDataRepository() = DataRepository()
-}
+
 
 
 
