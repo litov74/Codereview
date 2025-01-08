ba1a571187b4a0fc06e2c7d98137141372d1389f
Ermek <directoryourlabel@gmail.com>
2025-01-08 12:15:55 +0400

    BadDi2

--- app/src/main/java/com/codereview/VacanciesViewModel.kt
+++ app/src/main/java/com/codereview/VacanciesViewModel.kt
@@ -6,11 +6,13 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @HiltViewModel
 class VacanciesViewModel @Inject constructor(private val repo: DataRepository) : ViewModel() {
@@ -18,19 +20,25 @@ class VacanciesViewModel @Inject constructor(private val repo: DataRepository) :
     init{ result = repo.vacancies() }
 }
 
-interface DataRepository {
-    fun vacancies(): List<Vacancy>
+//interface DataRepository {
+//    fun vacancies(): List<Vacancy>
+//}
+class DataRepository {
+    fun vacancies() = getVacancies()
 }
-
-class DataRepoImpl@Inject constructor() : DataRepository {
-    override fun vacancies() = getVacancies()
-}
+//class DataRepoImpl@Inject constructor() : DataRepository {
+//    override fun vacancies() = getVacancies()
+//}
 
 @InstallIn(SingletonComponent::class)
 @Module
-interface ProfileModule {
-    @Binds
-    fun getProfileSource(repo: DataRepoImpl): DataRepository
+object ProfileModule {
+//    @Binds
+//    fun getProfileSource(repo: DataRepoImpl): DataRepository
+
+    @Provides
+    @Singleton
+    fun providesDataRepository() = DataRepository()
 }
 
 
