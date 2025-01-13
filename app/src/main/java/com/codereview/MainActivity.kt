f3280c279198855b8c868ec57224b9bf1d3a6249
Торгованов Л.Е. <l.torgovanov@evotor.ru>
2025-01-13 16:40:30 +0300

    Фиксы градла

--- app/src/main/java/com/codereview/MainActivity.kt
+++ app/src/main/java/com/codereview/MainActivity.kt
@@ -12,8 +12,6 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    //private val viewModel: VacanciesViewModel by viewModels()
-    //val viewModel: VacanciesViewModel = hiltViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
