ab0368e1e8a519b618479824cec597066fdc6548
xenia <xenia@test.ru>
2025-02-18 18:03:39 +0300

    Transforming data class JobSpec into enum class + changing JobRepositoryImpl getJobList() method

--- repository/src/main/java/com/codereview/repository/jobs_repository/JobRepositoryImpl.kt
+++ repository/src/main/java/com/codereview/repository/jobs_repository/JobRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.codereview.repository.jobs_repository
 
-import com.codereview.core.R
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -9,12 +8,12 @@ class JobRepositoryImpl @Inject constructor() : JobRepository {
 
     override fun getJobList(): Flow<List<JobSpec>> = flow {
         val jobList = listOf(
-            JobSpec("Python", "python", R.drawable.python),
-            JobSpec("Java", "java", R.drawable.java),
-            JobSpec("JavaScript", "javascript", R.drawable.js),
-            JobSpec("Data Science", "data", R.drawable.data_science),
-            JobSpec("QA", "qa", R.drawable.qa),
-            JobSpec("C#", "c%23", R.drawable.c_sharp)
+            JobSpec.PYTHON,
+            JobSpec.JAVA,
+            JobSpec.JAVASCRIPT,
+            JobSpec.DATASCIENCE,
+            JobSpec.QA,
+            JobSpec.CSHARP
         )
 
         emit(jobList)
--- repository/src/main/java/com/codereview/repository/jobs_repository/JobSpec.kt
+++ repository/src/main/java/com/codereview/repository/jobs_repository/JobSpec.kt
@@ -1,3 +1,12 @@
 package com.codereview.repository.jobs_repository
 
-data class JobSpec(val jobTitle: String, val specialities: String, val logoId: Int)
+import com.codereview.core.R
+
+enum class JobSpec(val jobTitle: String, val specialities: String, val logoId: Int) {
+    PYTHON(jobTitle = "Python", specialities = "python", R.drawable.python),
+    JAVA(jobTitle = "Java", specialities = "java", R.drawable.java),
+    JAVASCRIPT(jobTitle = "JavaScript", specialities = "javascript", R.drawable.js),
+    DATASCIENCE(jobTitle = "Data Science", specialities = "data", R.drawable.data_science),
+    QA(jobTitle = "QA", specialities = "qa", R.drawable.qa),
+    CSHARP(jobTitle = "C#", specialities = "c%23", R.drawable.c_sharp)
+}
